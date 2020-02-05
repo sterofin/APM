48cb5d6925e63419ea1651c82748ef8af8719742
sterofin <sterofinstophat@gmail.com>
2020-02-04 21:12:13 -0500

    fixed it so now the motors move at the same rate due to potentiometer input

--- motorControl/motorControl.ino
+++ motorControl/motorControl.ino
@@ -1,14 +1,9 @@
-/*  Arduino DC Motor Control - PWM | H-Bridge | L298N  -  Example 01
-
-    by Dejan Nedelkovski, www.HowToMechatronics.com
-*/
-
-#define enA 4
 #define in1 5
 #define in2 6
 #define in3 7
 #define in4 8
 #define enB 9
+#define enA 10
 
 void setup() {
   pinMode(enA, OUTPUT);
@@ -21,23 +16,19 @@ void setup() {
 }
 
 String power = "STOP";
+int pwmOutput, potValue;
+int maxVal = 255;
 void loop() {
-  int pwmOutputA, pwmOutputB;
-  int potValue;
-
   potValue = analogRead(A0); // Read potentiometer value
-  pwmOutputA = map(potValue, 0, 1023, 0 , 255); // Map the potentiometer value from 0 to 255
-  //pwmOutputB = map(potValue, 0, 1023, 100 , 355);
-
-  if (pwmOutputA < 120) {
-    pwmOutputA = -(pwmOutputA - 120)*2;
+  pwmOutput = map(potValue, 0, 1023, -maxVal , maxVal); // Map the potentiometer value from 0 to 255
+  if (pwmOutput < -10) {
     digitalWrite(in1, LOW);
     digitalWrite(in2, HIGH);
     digitalWrite(in3, LOW);
     digitalWrite(in4, HIGH);
     Serial.println("DOWN");
     delay(20);
-  } else if (pwmOutputA > 134) {
+  } else if (pwmOutput > 10) {
     digitalWrite(in1, HIGH);
     digitalWrite(in2, LOW);
     digitalWrite(in3, HIGH);
@@ -45,8 +36,8 @@ void loop() {
     Serial.println("UP");
     delay(20);
   } else {
-    analogWrite(enA, 127); // Send PWM signal to L298N Enable pin
-    analogWrite(enB, 127); // Send PWM signal to L298N Enable pin
+    analogWrite(enA, 0); // Send PWM signal to L298N Enable pin
+    analogWrite(enB, 0); // Send PWM signal to L298N Enable pin
     digitalWrite(in1, LOW);
     digitalWrite(in2, LOW);
     digitalWrite(in3, LOW);
@@ -54,7 +45,7 @@ void loop() {
     Serial.println("STOP");
   }
 
-  analogWrite(enA, pwmOutputA); // Send PWM signal to L298N Enable pin
-  //analogWrite(enB, pwmOutputB); // Send PWM signal to L298N Enable pin
-  Serial.println(pwmOutputA);
+  analogWrite(enA, min(abs(pwmOutput),255)); // Send PWM signal to L298N Enable pin
+  analogWrite(enB, min(abs(pwmOutput)+23,255)); // Send PWM signal to L298N Enable pin
+  Serial.println(pwmOutput);
 }
